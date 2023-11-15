f7ac533cfe7ffcb25298a9f5e3425930bd7c44d6
samane.saremi1992@gmail.com <samane.saremi1992@gmail.com>
2023-11-16 02:48:09 +0330

    change-step

--- app/[locale]/(infraction-form)/infraction/infractions-steps.tsx
+++ app/[locale]/(infraction-form)/infraction/infractions-steps.tsx
@@ -1,12 +1,9 @@
-'use client'
 import { useTranslations } from "next-intl";
 import Link from "next/link";
-import { usePathname } from 'next/navigation'
 
 const InfractionSteps = () =>{
 
     const t = useTranslations('Index');
-    const pathname = usePathname();    
     
     return(
         <section className="sm:flex sm:mt-14 mb-6 justify-center text-base">
@@ -20,15 +17,15 @@ const InfractionSteps = () =>{
                 </div>
             </Link>
             <Link href='/infraction' >
-                <div className={`${pathname === '/infraction' ? 'step-processing':'step-tobe-process'} flex mt-4 sm:mt-0 items-center justify-between sm:w-auto w-4/5 mx-auto`}>
-                    <span className={`${pathname === '/infraction' ? 'step-processing':'step-tobe-process'} steps material-symbols-outlined sm:ml-5 ml-2 relative p-1`}>Circle</span>
+                <div className="step-processing flex mt-4 sm:mt-0 items-center justify-between sm:w-auto w-4/5 mx-auto">
+                    <span className="step-processing steps material-symbols-outlined sm:ml-5 ml-2 relative p-1">Circle</span>
                     <p className="leading-10 ml-4 text-base font-medium">{t("infraction-step")}</p>
                     <hr className="sm:w-24 w-20 h-0.5 sm:ml-4 border-0 rounded"/>
                 </div>
             </Link>
             <Link href="/tracking">
-                <div className={`${pathname === '/tracking' ? 'step-processing':'step-tobe-process'} flex mt-4 sm:mt-0 items-center justify-between sm:w-auto w-4/5 mx-auto`}>
-                    <span className={`${pathname === '/tracking' ? 'step-processing':'step-tobe-process'} steps material-symbols-outlined relative sm:ml-5 ml-3 p-1`}>Circle</span>
+                <div className="step-tobe-process flex mt-4 sm:mt-0 items-center justify-between sm:w-auto w-4/5 mx-auto">
+                    <span className="step-tobe-process steps material-symbols-outlined relative sm:ml-5 ml-3 p-1">Circle</span>
                     <p className="leading-10 whitespace-nowrap ml-4 text-base font-medium">{t("tracking-step")}</p>
                     <hr className="sm:w-24 w-20 h-0.5 border-0 rounded"/>
                 </div>
